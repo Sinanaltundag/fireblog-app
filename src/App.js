08658fa5fb8df6ec8d74ac63a23e9c276688ccab
peykani <peykani@gmail.com>
2022-04-22 12:42:23 +0300

    activeuser control

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import { useEffect } from "react";
+import {  useDispatch } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./app-router/AppRouter";
 import "./App.css";
+import { setCurrentUser } from "./redux/actions/authActions";
+import { auth } from "./utils/firebase";
 
 function App() {
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const userInfo =  auth.onAuthStateChanged((user) => {
+      dispatch(setCurrentUser(user?.email));
+    });
+    return userInfo; //! clean-up function
+  }, [dispatch]);
+
+
   return (
     <div className="App">
+    
       <BrowserRouter>
         <AppRouter />
       </BrowserRouter>
+     
     </div>
   );
 }
