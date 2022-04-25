66bbc578c0988f35b09a07c617f43bfc157e43a9
peykani <peykani@gmail.com>
2022-04-25 04:48:39 +0300

    fix bug

--- src/app-router/AppRouter.jsx
+++ src/app-router/AppRouter.jsx
@@ -18,7 +18,7 @@ const AppRouter = () => {
 
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="about" element={<About />} />
+       
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
 
